6d201c45ff56a2fe235868c029bf8bd8798e19c7
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-07-13 13:26:14 +0200

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export K8S_HOST=kubernetes.default.svc
+export K8S_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+
+echo $K8S_HOST
+echo $K8S_TOKEN
+echo $K8S_NAMESPACE
