793ae66d012b308cad1b19baa17ac888ab4526e8
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-07-13 14:09:52 +0200

    Remove from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-echo $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 K8S_HOST=kubernetes.default.svc K8S_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) exec "$@"
