9f2d3c9b8dac6e475ca9d9f9cae543e687970c40
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-07-15 00:54:40 +0200

    Fix k8s url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export K8S_HOST=kubernetes.default.svc
+export K8S_HOST=http://kubernetes.default.svc
 export K8S_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
 exec "$@"
