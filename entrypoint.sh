6413d8c1b1eeb8ca76a2643e698055af63670600
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-07-15 01:04:56 +0200

    Fix kubernetes endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export K8S_HOST=http://kubernetes.default.svc
+export K8S_HOST="https://${KUBERNETES_SERVICE_HOST}"
 export K8S_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
 exec "$@"
