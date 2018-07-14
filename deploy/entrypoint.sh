4c8a36320b50993140cea5dedfe96b695648a98e
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-07-15 00:16:04 +0200

    Deployment image and notification

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+echo $GCLOUD_KEY_JSON | base64 -d > gcloud-service-key.json
+gcloud auth activate-service-account --key-file gcloud-service-key.json
+
+gcloud config set project $PROJECT_ID
+gcloud config set compute/zone $COMPUTE_ZONE
+gcloud container clusters get-credentials $CLUSTER_NAME
+
+kubectl set image deployment/api-server -n $CLUSTER_NAMESPACE api-server=$IMAGE_NAME
+kubectl set image deployment/deployment-runner -n $CLUSTER_NAMESPACE deployment-runner=$IMAGE_NAME
+kubectl set image deployment/job-status-cheker -n $CLUSTER_NAMESPACE job-status-cheker=$IMAGE_NAME
+kubectl set image deployment/slack-api-server -n $CLUSTER_NAMESPACE slack-api-server=$IMAGE_NAME
