b865543615f5e916c03d9570cedf2a79504bea6f
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-08-27 15:23:58 +0200

    Fix GraphQL for the frontend

--- src/GraphQL/Database/Queries.hs
+++ src/GraphQL/Database/Queries.hs
@@ -2,9 +2,11 @@ module GraphQL.Database.Queries
   ( module GraphQL.Database.Types
   , CompanyID
   , listBuilds
+  , listBuildsById
   , listEnvironments
   , listProjects
   , listProjectsById
+  , listEnvsLastDeployments
   ) where
 
 import RIO
@@ -22,14 +24,24 @@ listBuilds (limit, offset) companyId = runQuery q (companyId, limit, offset)
       "select b.id, b.name, b.params, b.metadata, b.project_id, cast(extract(epoch from b.created_at) as integer), cast(extract(epoch from b.updated_at) as integer) from builds b \
       \join projects p on p.id = b.project_id \
       \where p.company_id = ? \
-      \limit ? offset ?"
+      \limit ? offset ? \
+      \order by b.created_at desc"
+
+listBuildsById :: (HasPostgres m) => CompanyID -> [BuildID] -> m [Build]
+listBuildsById companyId ids = runQuery q (companyId, In ids)
+  where
+    q =
+      "select b.id, b.name, b.params, b.metadata, b.project_id, cast(extract(epoch from b.created_at) as integer), cast(extract(epoch from b.updated_at) as integer) from builds b \
+      \join projects p on p.id = b.project_id \
+      \where p.company_id = ? and b.id in ?"
 
 listProjects :: (HasPostgres m) => CompanyID -> m [Project]
 listProjects companyId = runQuery q (Only companyId)
   where
     q =
       "select id, name, deployment_image, access_token, cast(extract(epoch from created_at) as integer), cast(extract(epoch from updated_at) as integer) from projects \
-      \where company_id = ?"
+      \where company_id = ? \
+      \order by name asc"
 
 listProjectsById :: (HasPostgres m) => CompanyID -> [ProjectID] -> m [Project]
 listProjectsById companyId ids = runQuery q (companyId, In ids)
@@ -45,4 +57,16 @@ listEnvironments companyId projectIds = runQuery q (companyId, In projectIds)
     q =
       "select e.id, e.name, e.env_vars, e.project_id, cast(extract(epoch from e.created_at) as integer), cast(extract(epoch from e.updated_at) as integer) from environments e \
       \left join projects p on p.id = e.project_id \
-      \where p.company_id = ? and p.id in ?"
+      \where p.company_id = ? and p.id in ? \
+      \order by e.name asc"
+
+listEnvsLastDeployments ::
+     (HasPostgres m) => CompanyID -> [EnvironmentID] -> m [Deployment]
+listEnvsLastDeployments companyId envIds = runQuery q (companyId, In envIds)
+  where
+    q =
+      "select d.id, cast(extract(epoch from d.deployment_started_at) as integer), d.environment_id, d.build_id, d.status, cast(extract(epoch from e.created_at) as integer), cast(extract(epoch from e.updated_at) as integer) from deployments d \
+      \left join environments e on d.environment_id = e.id \
+      \left join projects p on e.project_id = p.id \
+      \where p.company_id = ? and e.id in ? \
+      \order by p.id, e.id, d.created_at desc"
