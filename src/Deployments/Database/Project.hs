641a0054d2939f90e5fc8a7cd9bf67f8a952525b
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2018-08-30 13:40:34 +0200

    Cleanup code

--- src/Deployments/Database/Project.hs
+++ src/Deployments/Database/Project.hs
@@ -4,7 +4,6 @@ module Deployments.Database.Project
   , getProject
   , getProjectForBuild
   , listProjects
-  , listProjectsById
   , updateProject
   ) where
 
@@ -15,16 +14,6 @@ import Database.PostgreSQL.Simple
 import Common.Database
 import qualified Deployments.Domain.Build as B
 import Deployments.Domain.Project
-import Util.Key (keyText)
-
-class PostgresProjectId a where
-  toId :: a -> Text
-
-instance PostgresProjectId Text where
-  toId = id
-
-instance PostgresProjectId ID where
-  toId = keyText
 
 createProject :: (HasPostgres m) => Project -> m ()
 createProject Project {..} = runDb' q values
@@ -53,16 +42,6 @@ listProjects companyId = map buildProject <$> runQuery q (Only companyId)
     q =
       "select id, name, deployment_image, company_id, access_token from projects\
         \ where company_id = ?"
-
-listProjectsById ::
-     (HasPostgres m, PostgresProjectId a) => CompanyID -> [a] -> m [Project]
-listProjectsById companyId projectIds =
-  map buildProject <$> runQuery q (In ids, companyId)
-  where
-    ids = map toId projectIds
-    q =
-      "select id, name, deployment_image, company_id, access_token from projects\
-        \ where id in ? and company_id = ?"
 
 getProject :: (HasPostgres m) => CompanyID -> Text -> m (Maybe Project)
 getProject companyId projectId = do
