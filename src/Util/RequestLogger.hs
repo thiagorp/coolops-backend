018a1e621a47965404706698ea1dd9bdd1bc402a
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2019-03-04 15:17:02 +0100

    Remove password from logs

---
+++ src/Util/RequestLogger.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Util.RequestLogger (jsonLogger) where
+
+import RIO
+import qualified RIO.ByteString as BS
+import qualified RIO.ByteString.Lazy as LBS
+import qualified RIO.HashMap as HM
+
+import Data.Aeson
+
+import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.RequestLogger.JSON
+
+jsonLogger :: OutputFormatterWithDetails
+jsonLogger date req status responseSize duration reqBody response =
+  formatAsJSON date req status responseSize duration (removeSensitiveInformation reqBody) response
+
+removeSensitiveInformation :: [ByteString] -> [ByteString]
+removeSensitiveInformation body = [ removePassword . BS.concat $ body ]
+
+removePassword :: ByteString -> ByteString
+removePassword body =
+  case decodeStrict body of
+    Just (Object jsonBody) ->
+      LBS.toStrict . encode $ HM.mapWithKey hideIfPassword jsonBody
+
+    _ ->
+      body
+
+hideIfPassword :: Text -> Value -> Value
+hideIfPassword "password" _ = String "[Password]"
+hideIfPassword _ v = v
